fad59440306cef0df7c977ecfcf83c8c7c7c4789
DasH <fxfddfno@gmail.com>
2020-02-08 11:16:56 +0200

    add preloading component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
+
 
 const Logs = () => {
     const [ logs , setLogs ] = useState([]);
@@ -22,7 +24,7 @@ const Logs = () => {
     }
 
     if(loading){
-        return <h4>Loading ...</h4>
+        return <Preloader />;
     }
 
 
