7bf6b253982f1f6ff92b3de942c4d6fb723b6354
DasH <fxfddfno@gmail.com>
2020-02-08 12:55:50 +0200

    change file src

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, Fragment } from 'react';
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
-import AddLogModel from "./components/layout/AddLogModal";
+import AddLogModel from "./components/logs/AddLogModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
