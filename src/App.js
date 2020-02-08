ae4b9cd73376629942eb97aaba27b77965ee168b
DasH <fxfddfno@gmail.com>
2020-02-08 10:17:29 +0200

    create Search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from "./components/layout/SearchBar";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -11,9 +12,9 @@ const App = ()=> {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      <h1>start</h1>
-    </div>
+    <Fragment>
+      <SearchBar />
+    </Fragment>
   );
 }
 
