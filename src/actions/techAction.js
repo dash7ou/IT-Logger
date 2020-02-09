02227e7e7af23992f24389db6a38affefba368f1
DasH <fxfddfno@gmail.com>
2020-02-09 11:03:12 +0200

    start with action tech

---
+++ src/actions/techAction.js
@@ -0,0 +1,25 @@
+import {
+    GET_TECHS,
+    ADD_TECH,
+    DELETE_TECH,
+    SET_LOADING,
+    TECHS_ERROR
+} from "./type";
+
+
+// Get techs from server...
+
+const getTechs = _=> async dispatch =>{
+    try{
+
+    }catch{
+
+    }
+};
+
+
+export const setLoading = _ => async dispatch =>{
+    dispatch({
+        type: SET_LOADING
+    })
+}
